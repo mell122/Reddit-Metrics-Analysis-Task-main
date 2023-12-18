117256c7847377465e18b992ac6f04f7dcd1647a
mell122 <94059072+mell122@users.noreply.github.com>
2023-12-18 16:53:12 -0500

    data gathering

--- src/scraper.py
+++ src/scraper.py
@@ -1,21 +1,50 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
+import datetime
+import praw
+import pandas as pd
+import os
+from dotenv import load_dotenv
 
-# Load data from CSV files
-dfs = {subreddit: pd.read_csv(f'{subreddit}_posts_data.csv') for subreddit in subreddits}
+load_dotenv()
 
-# Plotting
-for metric in ['title_length', 'upvotes', 'num_comments']:
-    plt.figure(figsize=(10, 6))
-    for subreddit, df in dfs.items():
-        sns.histplot(df[metric], kde=True, label=subreddit, alpha=0.5)
-    
-    plt.title(f'Distribution of {metric} across Subreddits')
-    plt.xlabel(metric)
-    plt.ylabel('Frequency')
-    plt.legend()
-    plt.show()
+# Loading variables from env
+env_client_id = os.getenv('client_id')
+env_client_secret = os.getenv('client_secret')
+env_user_agent = os.getenv('user_agent')
 
-# Additional analysis (e.g., comparing average post lengths)
-average_title_length = {subreddit: df['title_length'].mean() for subreddit, df in dfs.items()}
-print("Average Title Lengths:", average_title_length)
+# Set up Reddit API credentials
+reddit = praw.Reddit(client_id=env_client_id ,
+                     client_secret= env_client_secret ,
+                     user_agent= env_user_agent)
+
+# Specify subreddit and time period
+subreddits = ['emacs', 'vim']
+start_date = datetime.datetime.utcnow() - datetime.timedelta(days=90)  # 3 months ago
+end_date = datetime.datetime.utcnow()
+
+
+# Function to get top posts and extract metrics
+def get_top_posts(subreddit):
+    posts = []
+    for submission in reddit.subreddit(subreddit).top(time_filter='all', limit=None):
+        if start_date <= datetime.datetime.utcfromtimestamp(submission.created_utc) <= end_date:
+            post_metrics = {
+                'post_id': submission.id,
+                'title': submission.title,
+                'score': submission.score,
+                'num_comments': submission.num_comments,
+                'created_utc': submission.created_utc,
+                # Add more metrics as needed
+            }
+            posts.append(post_metrics)
+    return posts
+
+# Function to generate CSV file
+def generate_csv(subreddit, posts):
+    df = pd.DataFrame(posts)
+    path="../data"
+    df.to_csv(os.path.join(path, f'{subreddit}_posts.csv'), index=False)
+
+# Get top posts for each subreddit
+for subreddit in subreddits:
+    posts = get_top_posts(subreddit)
+    generate_csv(subreddit, posts)
